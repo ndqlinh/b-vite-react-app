76300d2f8c311c4b9313c7f3488fef122ff732a8
ndqlinh <ndqlinh36@gmail.com>
2024-05-10 17:22:04 +0700

    making sign in page

--- src/app/auth/Signin.tsx
+++ src/app/auth/Signin.tsx
@@ -1,6 +1,20 @@
 const Signin = () => {
   return (
-    <div>Signin Page</div>
+    <div className="login-page">
+      <div className="card">
+        <form className="form-login">
+          <div className="form-group">
+            <label htmlFor="username">User name / Email</label>
+            <input type="text" name="username" id="username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="text" name="password" id="password" />
+          </div>
+          <button className="btn">Sign in</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
