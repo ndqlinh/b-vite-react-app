ca25baa3244e43bb7771240232224070f3e3bed4
ndqlinh <ndqlinh36@gmail.com>
2024-05-15 10:46:12 +0700

    redirect to home if authenticated

--- src/app/auth/Auth.tsx
+++ src/app/auth/Auth.tsx
@@ -1,6 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import AuthHelper from '@core/helpers/auth.helper';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Auth = () => {
+  const navigate = useNavigate();
+  const auth = new AuthHelper();
+
+  useEffect(() => {
+    const isAuthenticated = auth.isAuthenticated();
+    if (isAuthenticated) {
+      navigate('/');
+    }
+  }, []);
+
   return (
     <Outlet />
   );
