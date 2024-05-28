5c4478932d69f184666d9f47efc927ca30f3d623
ndqlinh <ndqlinh36@gmail.com>
2024-05-28 16:29:20 +0700

    implement delete todo button

--- src/app/principal/components/TodoItem.tsx
+++ src/app/principal/components/TodoItem.tsx
@@ -1,4 +1,7 @@
+import moment from 'moment';
+
 import CheckboxIcon from '@assets/icons/checkbox.svg?react';
+import TrashIcon from '@assets/icons/trash.svg?react';
 
 const TodoItem = (props) => {
   const { todo } = props;
@@ -6,12 +9,22 @@ const TodoItem = (props) => {
   return (
     <li className="todo-item">
       <div className="card full-width">
-        <div className="checkbox">
-          <input type="checkbox" name="remember" id={ todo.id } className="checkbox-input" />
-          <label htmlFor={ todo.id } className="checkbox-label">
-            <span className="mr-3"><CheckboxIcon className="checked-icon" /></span>
-            <span>{ todo.title }</span>
-          </label>
+        <div className="flex flex-center-between">
+          <div className="card-content">
+            <div className="checkbox mb-1">
+              <input type="checkbox" name="remember" id={ todo.id } className="checkbox-input" />
+              <label htmlFor={ todo.id } className="checkbox-label">
+                <span className="mr-3"><CheckboxIcon className="checked-icon" /></span>
+                <span>{ todo.title }</span>
+              </label>
+            </div>
+            <p className="todo-due-date">{ moment(todo.dueDate).format('DD MMMM YYYY') }</p>
+          </div>
+          <div className="card-action">
+            <button className="btn btn-circle btn-danger">
+              <TrashIcon />
+            </button>
+          </div>
         </div>
       </div>
     </li>
