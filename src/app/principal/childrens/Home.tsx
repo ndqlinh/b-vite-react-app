3f15223ed65794e76d22798d26372a4e338dcf6b
ndqlinh <ndqlinh36@gmail.com>
2025-04-16 14:40:07 +0700

    style: Refactor Home component for improved readability and consistency in class names

--- src/app/principal/childrens/Home.tsx
+++ src/app/principal/childrens/Home.tsx
@@ -5,18 +5,21 @@ import ReactLogo from '@assets/icons/react.svg';
 const Home = () => {
   const [count, setCount] = useState(0);
 
-  const increaseCount = () => setCount(count => count + 1);
+  const increaseCount = () => setCount((count) => count + 1);
 
   return (
-    <div className="home-page flex-col flex-col-centered">
-      <img src={ ReactLogo } className="app-logo" alt="logo" />
+    <div className="flex flex-col items-center flex-1">
+      <img src={ReactLogo} className="h-[40vmin] mb-12" alt="logo" />
       <h1>Vite + React</h1>
-      <button className="btn btn-outline mb-3" onClick={increaseCount}>
+      <button
+        className="py-2.5 px-5 me-2 mb-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700"
+        onClick={increaseCount}
+      >
         count is {count}
       </button>
       <div>
         <a
-          className="txt-blue"
+          className="text-blue-500 underline"
           href="https://react.dev"
           target="_blank"
           rel="noopener noreferrer"
@@ -25,7 +28,7 @@ const Home = () => {
         </a>
         {' | '}
         <a
-          className="txt-blue"
+          className="text-blue-500 underline"
           href="https://vitejs.dev/guide/features.html"
           target="_blank"
           rel="noopener noreferrer"
@@ -34,7 +37,7 @@ const Home = () => {
         </a>
       </div>
     </div>
-  )
+  );
 };
 
 export default Home;
